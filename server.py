852041f1238ef7e26ec771769447424464d354ee
Milad <mi.zeraat@gmail.com>
2023-11-22 17:19:49 +0100

    explicitly defining the response number

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def hello_world():
     return 'Hello, World!'
 
+@app.route('/no_content')
+def no_content():
+    # Returning a tuple with a custom HTTP status code (204) and a JSON message
+    return jsonify({"message": "No content found"}), 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
